60d4d533ef3460d14bb4e1a43e807d0a315475e9
anajacimovic1995 <anajac95@gmail.com>
2016-03-08 13:19:22 +0100

    Dodata klasa Autor, klasa Knjiga refaktorisana da sadrzi listu autora

--- Biblioteka2016/src/biblioteka/Knjiga.java
+++ Biblioteka2016/src/biblioteka/Knjiga.java
@@ -1,13 +1,21 @@
 package biblioteka;
 
+import java.util.LinkedList;
+
 public class Knjiga {
 	
 	private String naslov;
 	private long isbn;
-	private String autori;
+	private LinkedList<Autor> autori;
 	private String izdavac;
 	private int izdanje;
 	
+	public LinkedList<Autor> getAutori() {
+		return autori;
+	}
+	public void setAutori(LinkedList<Autor> autori) {
+		this.autori = autori;
+	}
 	public String getNaslov() {
 		return naslov;
 	}
@@ -20,12 +28,7 @@ public class Knjiga {
 	public void setIsbn(long isbn) {
 		this.isbn = isbn;
 	}
-	public String getAutori() {
-		return autori;
-	}
-	public void setAutori(String autori) {
-		this.autori = autori;
-	}
+	
 	public String getIzdavac() {
 		return izdavac;
 	}
